11a3a747ccab2a8a384bb0694997fe2ba1732edb
LyK <lyk@LyK.LyK>
2019-01-09 20:41:40 +0100

    Ecris la structure du code dans code.py à l'aide de commentaires.

---
+++ code.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3.5
+
+#-*- coding: utf-8 -*-
+
+#Importer json
+
+
+#Affiche un message et demande le no
+#de l'utilisateur
+
+
+#Enregistre le nom de l'utilisateur dans
+#le fichier data.json
